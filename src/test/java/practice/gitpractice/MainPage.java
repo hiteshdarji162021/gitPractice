08dbd3f1f865ccca28394f0223bd66b421ec11fa
hiteshdarji162021 <hiteshdarji162021@gmail.com>
2025-03-20 16:10:13 +0530

    First commit

---
+++ src/test/java/practice/gitpractice/MainPage.java
@@ -0,0 +1,14 @@
+package practice.gitpractice;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+// page_url = https://www.jetbrains.com/
+public class MainPage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
